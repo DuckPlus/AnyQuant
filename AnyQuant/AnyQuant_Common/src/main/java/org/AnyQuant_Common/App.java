59597ee1918fe8c96ec2db80e2890f7f95dbd90e
czqInNanjing <czqofnju@gmail.com>
2016-03-02 09:20:22 +0800

    新建项目

---
+++ AnyQuant/AnyQuant_Common/src/main/java/org/AnyQuant_Common/App.java
@@ -0,0 +1,13 @@
+package org.AnyQuant_Common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ AnyQuant/src/main/java/Duck_Plus/AnyQuant/App.java
@@ -0,0 +1,13 @@
+package Duck_Plus.AnyQuant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
