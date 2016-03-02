f831e3e6e3a5d8e7b7de9c8a4190ac1c3c849653
czqInNanjing <czqofnju@gmail.com>
2016-03-02 15:54:30 +0800

    Development

---
+++ AnyQuant/AnyQuant_Client/src/main/java/ui/config/ParseXML.java
@@ -0,0 +1,64 @@
+package ui.config;
+
+import java.io.File;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+/**
+ * 对XML文件进行解析（读取修改）
+ * @author czq
+ * @version 2015年11月25日 下午3:32:05
+ */
+public class ParseXML {
+
+	
+	private Element root;
+	
+	private String pres = "docs//";
+	
+	private String xmlPath=  "UIConfig.xml";
+	
+	
+	
+	private SAXReader reader;
+	
+	private Document doc;
+
+	public ParseXML() {
+		init(xmlPath);
+	}
+	
+	public ParseXML(String xmlPath) {
+		init(xmlPath);
+	}
+	
+	private void init(String xmlPath){
+		
+		try {
+			 reader = new SAXReader();
+			 doc = reader.read(new File(pres + xmlPath));
+			 root = doc.getRootElement();
+			
+			
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		}
+	
+	public Element getConfig(String node){
+		return root.element(node);
+	}
+	
+	/**
+	 * 获得根节点
+	 * @return
+	 */
+	public Element getRoot(){
+		return root;
+	}
+}
