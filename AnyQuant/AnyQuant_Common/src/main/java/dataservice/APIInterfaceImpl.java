4bedebc75e00f2f8c92b38adb30e1286bcd3435b
njushishuo <675342907@qq.com>
2016-03-06 09:09:04 +0800

    development:

--- AnyQuant/AnyQuant_Common/src/main/java/dataservice/APIInterfaceImpl.java
+++ AnyQuant/AnyQuant_Common/src/main/java/dataservice/APIInterfaceImpl.java
@@ -151,7 +151,7 @@ public class APIInterfaceImpl implements APIInterface{
 		String startTime = preStart.DateToString();
 		String endTime = end.DateToString();
 		String url = "http://121.41.106.89:8010/api/stock/"+stockCode+"/?start="+startTime +"&end="+endTime+"&fields="+labels ;
-	    System.out.println(SendGET(url, ""));
+	  //  System.out.println(SendGET(url, ""));
 		JSONObject jo = JSONObject.fromObject(SendGET(url, ""));
 		JSONObject data = jo.getJSONObject("data");
 		JSONArray trading_info = data.getJSONArray("trading_info");
