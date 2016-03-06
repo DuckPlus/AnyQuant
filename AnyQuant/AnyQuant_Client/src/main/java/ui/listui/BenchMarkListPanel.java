da11802e06ee60387d204f969a23f7acd3ac140b
toastsgithub <623039975@qq.com>
2016-03-06 08:37:46 +0800

    Development:界面修改

--- AnyQuant/AnyQuant_Client/src/main/java/ui/listui/BenchMarkListPanel.java
+++ AnyQuant/AnyQuant_Client/src/main/java/ui/listui/BenchMarkListPanel.java
@@ -1,22 +1,27 @@
 package ui.listui;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Vector;
 
 import org.dom4j.Element;
 
 import ui.tool.MyPanel;
+import ui.tool.MyTable;
 
 /**
  * 大盘列表
- * @author czq
- * @date 2016年3月2日
+ * @author dzm
+ * @date 2016年3月4日
  */
 @SuppressWarnings("serial")
 public class BenchMarkListPanel extends MyPanel {
-
+	
+	MyTable BenchmarkListTable;
 	public BenchMarkListPanel(Element config) {
 		super(config);
-		// TODO Auto-generated constructor stub
+		this.setBackground(Color.lightGray);
+		initOtherCompoment(config.element("benchmarklistTable"));
 	}
 	
 	@Override
@@ -47,7 +52,38 @@ public class BenchMarkListPanel extends MyPanel {
 
 	@Override
 	protected void initOtherCompoment(Element e) {
-		// TODO Auto-generated method stub
+		Vector<String> vhead = new Vector<String>();
+		vhead.add("日期");
+		vhead.add("开盘价");
+		vhead.add("收盘价");
+		vhead.add("最高价");
+		vhead.add("最低价");
+		vhead.add("换手率");
+		vhead.add("成交量");
+		vhead.add("振幅");
+		vhead.add("变化率");
+		
+//		Element eBenchmark = e.element("benchmarklistTable")
+		BenchmarkListTable= new MyTable(Integer.valueOf(e.attributeValue("x")), 
+				Integer.valueOf(e.attributeValue("y")), 
+				Integer.valueOf(e.attributeValue("width")), 
+				Integer.valueOf(e.attributeValue("height")), vhead);
+		System.out.println(BenchmarkListTable.getBounds().getX());
+		System.out.println(BenchmarkListTable.getBounds().getY());
+		System.out.println(BenchmarkListTable.getBounds().getWidth());
+		System.out.println(BenchmarkListTable.getBounds().getHeight());
+		Vector<String>vd = new Vector<String>();
+		vd.add("data1");
+		vd.add("data1");
+		vd.add("data1");
+		vd.add("data1");
+		
+		BenchmarkListTable.addRow(vd);
+		BenchmarkListTable.addRow(vd);
+		BenchmarkListTable.addRow(vd);
+		BenchmarkListTable.addRow(vd);
+		BenchmarkListTable.addRow(vd);
+		this.add(BenchmarkListTable);
 
 	}
 
--- AnyQuant/AnyQuant_Client/src/main/java/ui/listui/StockListPanel.java
+++ AnyQuant/AnyQuant_Client/src/main/java/ui/listui/StockListPanel.java
@@ -4,14 +4,18 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Iterator;
 import java.util.Vector;
 
 import org.dom4j.Element;
 
+import blimpl.APIBlImpl;
+import blservice.APIBlservice;
 import ui.tool.MyPanel;
 import ui.tool.MyPictureButton;
 import ui.tool.MyTable;
 import ui.tool.MyTextField;
+import vo.StockVO;
 
 /**
  * 股票列表
@@ -23,6 +27,7 @@ public class StockListPanel extends MyPanel {
 	MyTable stocklistTable; 
 	MyTextField stockCodeInput;
 	MyPictureButton searchBtn;
+	APIBlservice apiBl;// = APIBlImpl.getAPIBLService();
 	public StockListPanel(Element config) {
 		super(config);
 		initOtherCompoment(config.element("stocklistTable"));
@@ -41,13 +46,39 @@ public class StockListPanel extends MyPanel {
 
 	@Override
 	protected void initButtons(Element e) {
+		//search Btn
 		searchBtn = new MyPictureButton(e.element("searchBtn"));
-//		searchBtn.setBounds(Integer.valueOf(e.attributeValue("x")), 
-//				Integer.valueOf(e.attributeValue("y")), 
-//				Integer.valueOf(e.attributeValue("width")), 
-//				Integer.valueOf(e.attributeValue("height")));
-		searchBtn.setBounds(320, 30, 100, 35);
+		searchBtn.setBounds(Integer.valueOf(e.element("searchBtn").attributeValue("x")), 
+				Integer.valueOf(e.element("searchBtn").attributeValue("y")), 
+				Integer.valueOf(e.element("searchBtn").attributeValue("width")), 
+				Integer.valueOf(e.element("searchBtn").attributeValue("height")));
 		this.add(searchBtn);
+		searchBtn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				String input = stockCodeInput.getText();
+				if(!input.equals("")){
+				Iterator<StockVO> itr = apiBl.getStocksByStockCode(input);
+				
+				while(itr.hasNext()){
+					StockVO temp = itr.next();
+					Vector<String>vData = new Vector<String>();
+					vData.add(temp.name);
+					vData.add(String.valueOf(temp.open));
+					vData.add(String.valueOf(temp.close));
+					vData.add(String.valueOf(temp.high));
+					vData.add(String.valueOf(temp.low));
+					vData.add(String.valueOf(temp.turnover));
+					vData.add(String.valueOf(temp.volume));
+					vData.add(String.valueOf(temp.amplitude));
+					vData.add(String.valueOf(temp.changeRate));
+					stocklistTable.addRow(vData);
+				}
+				
+				}
+				super.mousePressed(e);
+			}
+		});
 	}
 
 	@Override
@@ -70,9 +101,14 @@ public class StockListPanel extends MyPanel {
 	protected void initOtherCompoment(Element e) {
 		Vector<String> vhead = new Vector<String>();
 		vhead.add("股票代码");
-		vhead.add("index1");
-		vhead.add("index2");
-		vhead.add("index3");
+		vhead.add("开盘价");
+		vhead.add("收盘价");
+		vhead.add("最高价");
+		vhead.add("最低价");
+		vhead.add("换手率");
+		vhead.add("成交量");
+		vhead.add("振幅");
+		vhead.add("变化率");
 		
 		stocklistTable= new MyTable(Integer.valueOf(e.attributeValue("x")), 
 				Integer.valueOf(e.attributeValue("y")), 
