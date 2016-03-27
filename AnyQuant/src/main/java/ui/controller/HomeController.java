7076dacfddc9bbe182bcc4e0d6db3891954229de
njushishuo <675342907@qq.com>
2016-03-27 10:15:04 +0800

    development: other: solve conflict

--- AnyQuant/src/main/java/ui/controller/HomeController.java
+++ AnyQuant/src/main/java/ui/controller/HomeController.java
@@ -1,59 +1,7 @@
-<<<<<<< HEAD
 package ui.controller;
 
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import ui.GraphicsUtils;
-
-/**
- * Main controller of the whole pane
- *
- * @author Qiang
- * @date Mar 22, 2016
- */
-public class HomeController {
-
-	private LeftPaneController leftPaneController;
-	private RightPaneController rightPaneController;
-	private BottomEdgesController bottomEdgesController;
-	private SplitPane rootpane;
-	private AnchorPane leftPane;
-	private BorderPane rightPane;
-	private Pane bottomPane;
-
-	public HomeController(SplitPane rootpane) {
-		this.rootpane = rootpane;
-		initialPane();
-		//change the ratio of the left pane and the right pane
-		rootpane.setDividerPosition(0, 0.164); // 0.164 = 175/1080
-	}
-
-	private void initialPane() {
-
-		leftPane = (AnchorPane) GraphicsUtils.getParent("LeftPane");
-
-		rootpane.getItems().set(0, leftPane);
-
-		bottomPane = (Pane) GraphicsUtils.getParent("BottomPane");
-		rightPane = (BorderPane) GraphicsUtils.getParent("RightPane");
-		rootpane.getItems().set(1, rightPane);
-		rightPane.setBottom(bottomPane);
-		LeftPaneController.setPane(leftPane);
-		RightPaneController.setPane(rightPane);
-	}
-
-}
-=======
-package ui.controller;
-
-import javafx.collections.ListChangeListener.Change;
-import javafx.fxml.FXML;
-import javafx.scene.control.SplitPane;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -61,7 +9,7 @@ import ui.GraphicsUtils;
 
 /**
  * Main controller of the whole pane
- * 
+ *
  * @author Qiang
  * @date Mar 22, 2016
  */
@@ -85,11 +33,11 @@ public class HomeController {
 	}
 
 	private void initialPane() {
-		
+
 		leftPane = (AnchorPane) GraphicsUtils.getParent("LeftPane");
 
 		rootpane.getItems().set(0, leftPane);
-		
+
 		bottomPane = (Pane) GraphicsUtils.getParent("BottomPane");
 		rightPane = (BorderPane) GraphicsUtils.getParent("RightPane");
 		rootpane.getItems().set(1, rightPane);
@@ -98,14 +46,6 @@ public class HomeController {
 		RightPaneController.setPane(rightPane);
 		RightPaneController.setStage(stage);
 	}
-	
-	
 
-	
-	
-	
-	
-	
-	
+
 }
->>>>>>> branch 'master' of http://114.55.35.12/141250017_cseiii/Duck_plus.git
