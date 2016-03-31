ffeaedef79964b2ce8e04936dce536cf415cd495
njushishuo <675342907@qq.com>
2016-03-31 16:19:15 +0800

    development:那童年的希望是一台时光机,我可以一路开心到底都不换气---                     取消APIDataCache创建时自动更新股票最新列表的功能

--- AnyQuant/src/main/java/data/APIDataCache.java
+++ AnyQuant/src/main/java/data/APIDataCache.java
@@ -41,7 +41,7 @@ public class APIDataCache implements APIInterface {
 
 	public APIDataCache(APIInterface api) {
 		this.api = api;
-		updateLatestStockMes();
+	//	updateLatestStockMes();
 		updateSelectedStockInfo();
 
 	}
