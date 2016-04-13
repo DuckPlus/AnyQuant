42c7acef0f319c7199c3a8abf4080a37f5c4977c
njushishuo <675342907@qq.com>
2016-04-13 19:20:24 +0800

    .

--- AnyQuant/src/main/java/util/MyBarChart.java
+++ AnyQuant/src/main/java/util/MyBarChart.java
@@ -3,14 +3,11 @@ package util;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.python.antlr.PythonParser.return_stmt_return;
-
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Series;
-import vo.DealVO;
 
 public class MyBarChart {
 
@@ -28,10 +25,12 @@ public class MyBarChart {
 
 	public void addData(){
 		XYChart.Series series = new XYChart.Series();
+		this.barchart.getData().add(series);
+
 		for(Entry<String, Integer> entry:this.data.entrySet()){
+
         	series.getData().add(new XYChart.Data(entry.getKey(),entry.getValue()));
         }
-		this.barchart.getData().add(series);
 	}
 
 	public BarChart<String, Number> createBarChart(Map<String , Integer>  data){
