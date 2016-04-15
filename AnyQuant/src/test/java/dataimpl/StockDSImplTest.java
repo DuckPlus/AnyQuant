851a9182875a0d35388501e678b9d846d61477d9
njushishuo <675342907@qq.com>
2016-04-15 15:56:07 +0800

    development:创建数据层测试类，移动了旧的测试类的位置

---
+++ AnyQuant/src/test/java/dataimpl/StockDSImplTest.java
@@ -0,0 +1,74 @@
+package dataimpl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author ss
+ * @date 2016年4月15日
+ */
+public class StockDSImplTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetAllStocks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllStocksInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllStocksExchange() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllStocksIntExchange() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStockMesString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStockMesStringMyDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStockMesStringMyDateMyDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllStockMes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTimeSharingPOs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateAllMes() {
+		fail("Not yet implemented");
+	}
+
+}
