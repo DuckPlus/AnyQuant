b16f924a2e8e292222b841fd0a318a278ab0dabd
njushishuo <675342907@qq.com>
2016-04-15 16:32:51 +0800

    development:写测试

--- AnyQuant/src/test/java/dataimpl/StockDSImplTest.java
+++ AnyQuant/src/test/java/dataimpl/StockDSImplTest.java
@@ -2,9 +2,18 @@ package dataimpl;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import data.StockDSImpl;
+import dataservice.StockDataService;
+import enumeration.Exchange;
+import jnr.ffi.Struct.int16_t;
+import po.StockPO;
 
 /**
  *
@@ -13,37 +22,103 @@ import org.junit.Test;
  */
 public class StockDSImplTest {
 
+	StockDataService stockDataService;
+
 	@Before
 	public void setUp() throws Exception {
+		stockDataService = StockDSImpl.getStockDSImpl();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		stockDataService = null;
 	}
 
 	@Test
 	public void testGetAllStocks() {
-		fail("Not yet implemented");
+		List<String> codeStrings = stockDataService.getAllStocks();
+		if (codeStrings == null) {
+			fail("testGetAllStocks()--null");
+		} else if (codeStrings.size() < 10) {
+			fail("testGetAllStocks()--size");
+		}
 	}
 
 	@Test
 	public void testGetAllStocksInt() {
-		fail("Not yet implemented");
+		for (int i = 2010; i < 2017; i++) {
+			List<String> codeStrings = stockDataService.getAllStocks(i);
+			if (codeStrings == null) {
+				fail("testGetAllStocks()--null");
+			} else if (codeStrings.size() < 10) {
+				fail("testGetAllStocks()--size");
+			}
+		}
 	}
 
 	@Test
 	public void testGetAllStocksExchange() {
-		fail("Not yet implemented");
+		List<String> codes = stockDataService.getAllStocks(Exchange.sh);
+		if (codes == null) {
+			fail("testGetAllStocks()--null");
+		} else if (codes.size() < 10) {
+			fail("testGetAllStocks()--size");
+		}
+
+		List<String> codeStrings = stockDataService.getAllStocks(Exchange.sz);
+		if (codeStrings == null) {
+			fail("testGetAllStocks()--null");
+		} else if (codeStrings.size() < 10) {
+			fail("testGetAllStocks()--size");
+		}
+
 	}
 
 	@Test
 	public void testGetAllStocksIntExchange() {
-		fail("Not yet implemented");
+		List<String> codes;
+
+		for (int i = 2015; i < 2018; i++) {
+			for (int j = 0; j < 2; j++) {
+
+				if (j % 2 == 0) {
+					codes = stockDataService.getAllStocks(Exchange.sh);
+				} else {
+					codes = stockDataService.getAllStocks(Exchange.sz);
+				}
+
+				if (codes == null) {
+					fail("testGetAllStocks()--null");
+				} else if (codes.size() < 10) {
+					fail("testGetAllStocks()--size");
+				}
+			}
+		}
+
 	}
 
 	@Test
 	public void testGetStockMesString() {
-		fail("Not yet implemented");
+		String [] codes = {"sh600000","sh600216","sz002588","sz002310","sh00012","sh5000001"};
+		List<String> errorCode = new ArrayList<String>();
+		List<StockPO>  stockPOs = new ArrayList<>();
+		for(int i=0; i<codes.length;i++){
+			StockPO stockpo =  stockDataService.getStockMes(codes[i]);
+			if(stockpo==null){
+				errorCode.add(codes[i]);
+			}else{
+				stockPOs.add(stockpo);
+			}
+		}
+
+		if(!errorCode.get(0).equals(codes[4])||!errorCode.get(1).equals(codes[5])){
+			fail("testGetStockMesString()---errorCodes can't be properly handled");
+		}else if( !stockPOs.get(0).getCode().equals(codes[0])){
+			fail("testGetStockMesString()---stockpo code changed");
+		}
+
+
+
 	}
 
 	@Test
