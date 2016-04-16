ee54aa3d17a1156aaabb997f0be5edf8548655ac
njushishuo <675342907@qq.com>
2016-04-16 13:00:52 +0800

    development:完成所有数据层的单元测试

--- AnyQuant/src/test/java/dataimpl/OptionalStockDSImplTest.java
+++ AnyQuant/src/test/java/dataimpl/OptionalStockDSImplTest.java
@@ -2,15 +2,18 @@ package dataimpl;
 
 import static org.junit.Assert.*;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.python.antlr.PythonParser.else_clause_return;
 import org.python.antlr.op.Add;
 
 import data.OptionalStockDSImpl;
 import dataservice.OptionalStockDataService;
+import po.StockPO;
 
 /**
  *
@@ -18,7 +21,8 @@ import dataservice.OptionalStockDataService;
  * @date 2016年4月15日
  */
 public class OptionalStockDSImplTest {
-    OptionalStockDataService dataservice ;
+	OptionalStockDataService dataservice;
+
 	@Before
 	public void setUp() throws Exception {
 		dataservice = OptionalStockDSImpl.getOptionalStockDSImpl();
@@ -26,45 +30,74 @@ public class OptionalStockDSImplTest {
 
 	@After
 	public void tearDown() throws Exception {
-		dataservice=null;
+		dataservice = null;
+	}
+
+	@Test
+	public void testGetSelectedStockCodes() {
+		List<String> codes = dataservice.getSelectedStockCodes();
+		if (codes == null) {
+			fail(" testGetSelectedStockCodes()--null");
+		}
+	}
+
+	@Test
+	public void testGetOptionalStocks() {
+		Iterator<StockPO> poIterator = dataservice.getOptionalStocks();
+		if (poIterator == null) {
+			fail("testGetOptionalStocks()--null");
+		} else if (!poIterator.hasNext()) {
+			fail("testGetOptionalStocks()--size");
+		}
 	}
 
 	@Test
 	public void testDeleteOptionalStock() {
-		String codes [] = {"sh600023","sh600021", "sh50012"};
-		if(!dataservice.deleteOptionalStock(codes[0])){
-			 fail("testDeleteOptionalStock()--can't delete code that does exist!");
-		}else{
-			if(dataservice.deleteOptionalStock(codes[0])){
-				 fail("testDeleteOptionalStock()--try to delete code that doesn't exist!");
+		String codes[] = { "sh600023", "sh600021", "sh50012" };
+		if (!dataservice.deleteOptionalStock(codes[0])) {
+			fail("testDeleteOptionalStock()--can't delete code t" + "hat does exist!");
+		} else {
+			if (dataservice.deleteOptionalStock(codes[0])) {
+				fail("testDeleteOptionalStock()--try to delete code that doesn't exist!");
 			}
 		}
 
 		dataservice.addOptionalStock(codes[0]);
 
-		if(dataservice.deleteOptionalStock(codes[2])){
-			 fail("testDeleteOptionalStock()-- try to delete code that doesn't exist!");
+		if (dataservice.deleteOptionalStock(codes[2])) {
+			fail("testDeleteOptionalStock()-- try to delete code that doesn't exist!");
 		}
 	}
 
 	@Test
 	public void testAddOptionalStock() {
-		fail("Not yet implemented");
+		String code = "sz002592";
+		dataservice.addOptionalStock(code);
+		List<String> codes = dataservice.getSelectedStockCodes();
+		if (!codes.contains(code)) {
+			fail("testAddOptionalStock()--fail to add stock");
+		} else {
+			dataservice.deleteOptionalStock(code);
+		}
+
 	}
 
 	@Test
 	public void testClearOptionalStocks() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	public void testGetSelectedStockCodes() {
-		fail("Not yet implemented");
-	}
+		dataservice.clearOptionalStocks();
+		List<String> codes = dataservice.getSelectedStockCodes();
+		if (codes == null) {
+			fail("testClearOptionalStocks()--null");
+		} else if (codes.size() != 0) {
+			fail("testClearOptionalStocks()--size");
+		}
 
-	@Test
-	public void testGetOptionalStocks() {
-		fail("Not yet implemented");
+		String oldCodes[] = { "sh600012", "sh600019", "sh600028", "sh600048", "sh600007", "sh600018", "sh600026",
+				"sh600016", "sh600021", "sh600023" };
+		for(String code:oldCodes){
+			dataservice.addOptionalStock(code);
+		}
 	}
 
 }
