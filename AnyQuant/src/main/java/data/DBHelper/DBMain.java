1c77c0929a4e1dffc17f0fbbe0043f44d1b2440f
njushishuo <675342907@qq.com>
2016-05-04 10:09:41 +0800

    development:数据库的搭建

---
+++ AnyQuant/src/main/java/data/DBHelper/DBMain.java
@@ -0,0 +1,15 @@
+package data.DBHelper;
+/**
+*
+* @author ss
+* @date 2016年4月22日
+*/
+public class DBMain {
+
+	public  static void main(String args[]){
+          DataBaseHelper helper = new DataBaseHelper();
+      
+       helper.initStocks();
+	}
+
+}
