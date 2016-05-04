1357ce5aa75cbcdd41b3470c417b735aa2848d5c
czqInNanjing <czqofnju@gmail.com>
2016-05-04 21:03:40 +0800

    Commit again

---
+++ AnyQuant_SSH/src/main/java/DAO/impl/UserDAOImpl.java
@@ -0,0 +1,43 @@
+package DAO.impl;
+
+import DAO.BaseDAO;
+import DAO.UserDAO;
+import entity.UserEntity;
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author Qiang
+ * @date 16/5/4
+ */
+@Repository
+public class UserDAOImpl implements UserDAO {
+
+    @Autowired
+    private BaseDAO baseDao;
+
+
+    public void save(UserEntity u) {
+        baseDao.save(u);
+    }
+
+    public List<UserEntity> findAll() {
+
+        return (List<UserEntity>) baseDao.getAllList(UserEntity.class);
+
+    }
+
+    @Override
+    public boolean checkIfValid(UserEntity u) {
+        UserEntity entity = (UserEntity) baseDao.load(UserEntity.class , u.getId());
+        if(entity.getName().equals(u.getName())){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
