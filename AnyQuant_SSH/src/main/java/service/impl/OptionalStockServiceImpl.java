c5f10883f5212962d21146c97a24c368f8355a87
czqInNanjing <czqofnju@gmail.com>
2016-05-10 19:53:10 +0800

    Development:完善逻辑层,增加自选股接口

--- AnyQuant_SSH/src/main/java/service/impl/OptionalStockServiceImpl.java
+++ AnyQuant_SSH/src/main/java/service/impl/OptionalStockServiceImpl.java
@@ -2,6 +2,8 @@ package service.impl;
 
 import entity.StockEntity;
 import entity.StockdataEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import service.OptionalStockService;
 
 import java.util.HashMap;
@@ -10,103 +12,113 @@ import java.util.Map;
 
 /**
  * @author Qiang
- * @date 16/5/8
+ * @date 16/5/10
  */
+@Service
+@Transactional
 public class OptionalStockServiceImpl implements OptionalStockService {
     /**
      * Get today's(or last trading day)data of the optional stockCodes
      *
+     * @param userID
      * @return a Collection's Iterator on StockVOs
      */
     @Override
-    public List<StockdataEntity> getOptionalStocksDatas() {
+    public List<StockdataEntity> getOptionalStocksDatas(String userID) {
         return null;
     }
 
     /**
      * Get  the optional stocks
      *
-     * @return a list
+     * @param userID
+     * @return a Collection's Iterator on StockVOs
      */
     @Override
-    public List<StockEntity> getOptionalStocks() {
+    public List<StockEntity> getOptionalStocks(String userID) {
         return null;
     }
-
 
     /**
      * delete optional stocks
      *
+     * @param userID
      * @param stockCode the stock to delete
      * @return if not exist, return false , if success return true
      */
     @Override
-    public boolean deleteStockCode(String stockCode) {
+    public boolean deleteStockCode(String userID, String stockCode) {
         return false;
     }
 
     /**
      * delete optional stocks
      *
+     * @param userID
      * @param stockCode the list of the stockCodes to delete
      * @return if not exist, return false , if success return true
      */
     @Override
-    public boolean deleteStockCode(List<String> stockCode) {
+    public boolean deleteStockCode(String userID, List<String> stockCode) {
         return false;
     }
 
     /**
      * add optional stocks
      *
+     * @param userID
      * @param stockCode the stock to add
      * @return if this stock has existed in the optional list or the stock not actually exists , return false
      */
     @Override
-    public boolean addStockCode(String stockCode) {
+    public boolean addStockCode(String userID, String stockCode) {
         return false;
     }
 
     /**
      * add optional stocks
      *
+     * @param userID
      * @param stockCodes the list of stock to add
      * @return if this stock has existed in the optional list or the stock not actually exists , return false
      */
     @Override
-    public boolean addStockCode(List<String> stockCodes) {
+    public boolean addStockCode(String userID, List<String> stockCodes) {
         return false;
     }
 
     /**
      * clear all the optional stocks
      *
+     * @param userID
      * @return success or fail
      */
     @Override
-    public boolean clearOptionalStocks() {
+    public boolean clearOptionalStocks(String userID) {
         return false;
     }
 
     /**
      * if the stock exist return true
      *
+     * @param userID
      * @param stockCode
      * @return
      */
     @Override
-    public boolean ifStockExist(String stockCode) {
+    public boolean ifStockExist(String userID, String stockCode) {
         return false;
     }
 
     /**
      * Get the region's distribution of the optional stocks
      *
+     * @param userID
      * @return map
      */
     @Override
-    public Map<String, Integer> getRegionDistributionMap() {
-        List<StockEntity> optionStocks = getOptionalStocks();
+    public Map<String, Integer> getRegionDistributionMap(String userID) {
+        List<StockEntity> optionStocks = getOptionalStocks(userID);
         Map<String, Integer> regions = new HashMap<>(34);
 
         for (StockEntity temp : optionStocks) {
@@ -123,10 +135,11 @@ public class OptionalStockServiceImpl implements OptionalStockService {
     /**
      * Get the board's distribution of the optional stocks
      *
+     * @param userID
      * @return map
      */
     @Override
-    public Map<String, Integer> getBoardDistributionMap() {
+    public Map<String, Integer> getBoardDistributionMap(String userID) {
         return null;
     }
 }
