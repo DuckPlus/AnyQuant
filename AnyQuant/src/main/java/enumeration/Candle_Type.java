fbb8ea6ce6783ade561d5b4c3d894950d0453cd0
czqInNanjing <czqofnju@gmail.com>
2016-05-10 20:09:42 +0800

    Development：删除原分层模式版本,该版本保留在迭代一和迭代二的分支

--- AnyQuant/src/main/java/enumeration/Candle_Type.java
+++
@@ -1,9 +0,0 @@
-package enumeration;
-/**
- *
- * @author ss
- * @date 2016年4月12日
- */
-public enum Candle_Type {
-       day,week,month
-}
--- AnyQuant/src/main/java/ui/python/WorkWithPython.java
+++
@@ -1,9 +0,0 @@
-package ui.python;
-/**
- * 实现与Python的协作
- * @author czq
- * @date 2016年3月2日
- */
-public class WorkWithPython {
-
-}
