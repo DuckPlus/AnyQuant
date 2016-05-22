ebd53da9d53c327bb0b15839e95d64f6f97fad74
njushishuo <675342907@qq.com>
2016-05-22 11:17:29 +0800

    development: complete factor DAO

--- AnyQuant_SSH/src/main/java/DAO/impl/FactorDAOImpl.java
+++ AnyQuant_SSH/src/main/java/DAO/impl/FactorDAOImpl.java
@@ -1,25 +1,57 @@
 package DAO.impl;
 
+import DAO.BaseDAO;
 import DAO.FactorDAO;
 import entity.FactorEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import util.DateCalculator;
 import util.MyDate;
+import util.ReflectHelper;
 import util.enumration.AnalysisFactor;
 import vo.Factor_VO;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author ss
  * @date 16/5/20
  */
+@Repository
 public class FactorDAOImpl implements FactorDAO {
+
+    @Autowired
+    BaseDAO baseDAO;
+
+    private static String tableName = FactorEntity.class.getName();
+
     @Override
     public List<Factor_VO> getFactorVO(String stockCode, AnalysisFactor factor, MyDate start, MyDate end) {
-        return null;
+        List<FactorEntity> tempList = getFactorByDate(stockCode,start,end);
+        List<Factor_VO> result = new ArrayList<>();
+        for(FactorEntity entity:tempList){
+            Factor_VO vo = new Factor_VO();
+            vo.name = factor.chinese;
+            vo.value= (double)
+                    ReflectHelper.getValueByAttrName(entity,factor.name());
+
+            vo.date= DateCalculator.SQLDateToMyDate(entity.getDate());
+            result.add(vo);
+        }
+        return result;
     }
 
     @Override
-    public List<FactorEntity> getFactorByDate(String stockCode, MyDate start, MyDate end) {
-        return null;
+    public List<FactorEntity> getFactorByDate(String stockCode, MyDate start, MyDate end)
+    {
+
+        String hql = "from "+tableName+
+                " where code = '"+stockCode+"' and date between '"+start.DateToString()+"' and '"+end.DateToString()+"'";
+
+        return (List<FactorEntity>) baseDAO.getAllList(hql);
     }
+
+
+
 }
---
+++ AnyQuant_SSH/src/test/java/DAO/impl/FactorDAOImplTest.java
@@ -0,0 +1,21 @@
+package DAO.impl;
+
+import org.junit.Test;
+
+/**
+ * Created by 67534 on 2016/5/22.
+ */
+
+
+public class FactorDAOImplTest {
+    @Test
+    public void getFactorVO() throws Exception {
+
+    }
+
+    @Test
+    public void getFactorByDate() throws Exception {
+
+    }
+
+}
