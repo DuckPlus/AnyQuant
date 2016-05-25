9e0b7c69c25e29d0c23f8d79d1c6b091f511c769
njushishuo <675342907@qq.com>
2016-05-25 16:23:14 +0800

    development

--- AnyQuant_SSH/src/main/java/service/impl/StrategyServiceImpl.java
+++ AnyQuant_SSH/src/main/java/service/impl/StrategyServiceImpl.java
@@ -1,17 +1,19 @@
 package service.impl;
 
 import service.StrategyService;
+import vo.ReportVO;
 import vo.StrategyArgumentVO;
-
-import javax.security.auth.callback.Callback;
 
 /**
  * @author Qiang
  * @date 16/5/22
  */
 public class StrategyServiceImpl implements StrategyService {
+
     @Override
-    public void doAnalysis(StrategyArgumentVO vo, Callback handler) {
+    public ReportVO doAnalysis(StrategyArgumentVO vo) {
+
+        return null;
 
     }
 }
