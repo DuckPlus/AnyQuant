3ea5b354031dc7c60a019406c4bb5c254f971141
flyawaygh <776550026@qq.com>
2016-05-25 10:04:06 +0800

    development:板块列表的模板先挂上去

--- AnyQuant_SSH/src/main/java/controller/MockController.java
+++ AnyQuant_SSH/src/main/java/controller/MockController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import service.BenchMarkService;
 
 import util.MyDate;
+import vo.CompareBoardAndBenchVO;
 
+import java.util.ArrayList;
 import java.util.List;
 /**
  * Created by dsn on 2016/5/22.
@@ -18,10 +20,18 @@ import java.util.List;
 @Controller
 @RequestMapping("/Mock")
 public class MockController {
-
-    @RequestMapping("/getMockStockList")
+    /**
+     * 获得沪深300和板块的对比数据,默认返回半年的数据
+     */
+    @RequestMapping("/getCompareData")
     @ResponseBody
-    public void getMockBoardDistribution(){
-
+    public List<CompareBoardAndBenchVO> getBoardAndBenchChartData(String boardName){
+       List<CompareBoardAndBenchVO> ans=new ArrayList<>(180);
+        for(int i=0;i<30;i++){
+            CompareBoardAndBenchVO vo= new CompareBoardAndBenchVO(new MyDate(2016,1,i),"钢铁",i*10,i*10+2);
+            ans.add(vo);
+        }
+        return ans;
     }
+
 }
