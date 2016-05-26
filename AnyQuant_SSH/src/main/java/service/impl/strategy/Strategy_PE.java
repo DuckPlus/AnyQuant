37ee9db1ad6ba042868a108d875e5d92df8f9ba6
njushishuo <675342907@qq.com>
2016-05-26 09:44:49 +0800

    development

--- AnyQuant_SSH/src/main/java/service/impl/strategy/Strategy_PE.java
+++ AnyQuant_SSH/src/main/java/service/impl/strategy/Strategy_PE.java
@@ -1,6 +1,10 @@
 package service.impl.strategy;
 
+import util.MyDate;
 import vo.ReportVO;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by 67534 on 2016/5/25.
@@ -10,14 +14,27 @@ public class Strategy_PE extends BaseStrategy {
      * 调仓间隔
      */
     public int interval;
+    /**
+     * 股票池
+     */
+    public List<String> stocks;
 
-    public Strategy_PE(int interval){
-        this.interval = interval;
+    public Strategy_PE(){
+        super();
     }
+    public Strategy_PE(double capital, double taxRate, String baseCode ,
+                       MyDate start , MyDate end , int interval ){
+        super(capital,taxRate,baseCode,start,end);
+        this.interval=interval;
+        this.stocks = new ArrayList<>();
+    }
+
+
 
 
     @Override
     public void init() {
+
 
     }
 
--- AnyQuant_SSH/src/main/java/vo/ReportVO.java
+++ AnyQuant_SSH/src/main/java/vo/ReportVO.java
@@ -1,7 +1,12 @@
 package vo;
 
+import java.util.List;
+
 /**
  * Created by 67534 on 2016/5/25.
  */
 public class ReportVO {
+
+    public List<CumRtnVO> cumRtnVOList;
+
 }
