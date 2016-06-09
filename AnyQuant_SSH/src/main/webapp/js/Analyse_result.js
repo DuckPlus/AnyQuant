79a1d425f16bfc820c7460013c58422d9dd017d1
toastsgithub <623039975@qq.com>
2016-06-09 13:59:02 +0800

    x

--- AnyQuant_SSH/src/main/webapp/js/Analyse_result.js
+++ AnyQuant_SSH/src/main/webapp/js/Analyse_result.js
@@ -4,20 +4,51 @@
 
 $(document).ready(function () {
     // alert("ready");
-    var data="{}";
-
-    init_factor_table(data);   
+    var data=
+        [
+        {
+            "factor":"FACTOR2",
+            "weight":0.2
+        },
+        {
+           "factor":"FACTOR1",
+           "weight":0.5
+        }, 
+            {
+                "factor":"FACTOR3",
+                "weight":0.3
+            }
+    ];
+    var data2 = 
+    [
+        {"stock_name":"stock 1","stock_code":"sh600001"},
+        {"stock_name":"stock 2","stock_code":"sh600002"},
+        {"stock_name":"stock 1","stock_code":"sh600001"},
+        {"stock_name":"stock 2","stock_code":"sh600002"},
+        {"stock_name":"stock 1","stock_code":"sh600001"},
+        {"stock_name":"stock 2","stock_code":"sh600002"},
+        {"stock_name":"stock 1","stock_code":"sh600001"},
+        {"stock_name":"stock 2","stock_code":"sh600002"},
+        {"stock_name":"stock 1","stock_code":"sh600001"},
+        {"stock_name":"stock 2","stock_code":"sh600002"},
+        {"stock_name":"stock 3","stock_code":"sh600003"}
+    ];
+    
+    init_factor_table(data);
+    init_stock_pool_table(data2);
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+        init_compare_chart(data)
+    });//
 });
 
 function init_factor_table(allStock) {
-    alert(allStock);
-    var table = $('#factor_weight').DataTable( {
+    var table = $('#factor_weight_table').DataTable( {
 
         data:allStock,
         lengthChange:false,
         pageLength:10,
-        dom: 'lrtip',
-        "order":[[1,"asc"]],
+        dom: 'lrtp',
+        // "order":[[1,"asc"]],
         columns:[
             {data:'factor'},
             {data:'weight'}
@@ -25,7 +56,7 @@ function init_factor_table(allStock) {
         /*
          *自适应宽度 默认true 关闭可提升性能,同时方便管理布局
          */
-        "autoWidth":false,
+        // "autoWidth":false,
         //国际化
         "oLanguage": {
             "sProcessing": "疯狂加载数据中.",
@@ -34,7 +65,6 @@ function init_factor_table(allStock) {
             "sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
             "sInfoEmpty": "没有数据",
             "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
-            "sZeroRecords": "没有检索到数据",
             "sSearch": "模糊查询:  ",
             "oPaginate": {
                 "sFirst": "首页",
@@ -44,5 +74,64 @@ function init_factor_table(allStock) {
             }
         }
     } );
+}
+function init_stock_pool_table(allStock) {
+    var table = $('#stock_pool_table').DataTable( {
+
+        data:allStock,
+        lengthChange:false,
+        pageLength:10,
+        dom: 'lrtp',
+        // "order":[[1,"asc"]],
+        columns:[
+            {data:'stock_name'},
+            {data:'stock_code'}
+        ],
+        /*
+         *自适应宽度 默认true 关闭可提升性能,同时方便管理布局
+         */
+        // "autoWidth":false,
+        //国际化
+        "oLanguage": {
+            "sProcessing": "疯狂加载数据中.",
+            "sLengthMenu": "每页显示 _MENU_ 条记录",
+            "sZeroRecords": "抱歉， 没有找到",
+            "sInfo": "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+            "sInfoEmpty": "没有数据",
+            "sInfoFiltered": "(从 _MAX_ 条数据中检索)",
+            "sSearch": "模糊查询:  ",
+            "oPaginate": {
+                "sFirst": "首页",
+                "sPrevious": "前一页",
+                "sNext": "后一页",
+                "sLast": "尾页"
+            }
+        }
+    } );
+}
+function init_compare_chart(data) {
+        
+            // Create the chart
+            $('#compare_chart').highcharts('StockChart', {
+
+
+                rangeSelector : {
+                    selected : 1
+                },
+
+                title : {
+                    text : 'AAPL Stock Price'
+                },
+
+                series : [{
+                    name : 'AAPL',
+                    data : data,
+                    tooltip: {
+                        valueDecimals: 2
+                    }
+                }]
+            });
+        
+
 
 }
---
+++ AnyQuant_SSH/src/main/webapp/jsp/index_new.jsp
@@ -0,0 +1,45 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %><!Doctype = html>
+<html>
+
+<head>
+    <style>
+
+    </style>
+
+    <%--tab上的小图标--%>
+    <link rel="shortcut icon" href="/image/short_cut.png">
+    <link rel="stylesheet" type="text/css" href="/css/login_new.css">
+    <script src="../js/login.js"></script>
+</head>
+
+<body>
+    <h1 style="font-family: 'Kozuka Gothic Pro'" class="home_title">
+        Duck Quantitative Transaction
+    </h1>
+    <div id="login_frame">
+        <fieldset id="loginModule" style="margin-left: auto;margin-right: auto;">
+
+            <form action="/user/login" method="post" autocomplete="off">
+                <div id="user_icon"></div>
+            <div id="account_with_icon">
+                <img src="/image/login_white.png" alt="ACC" id="account_icon">
+                <input id="username" name="username" type="text" placeholder="username" class="flat_input_text" required>
+            </div>
+            <div id="password_with_icon">
+                <img src="/image/password_white.png" alt="PAS" id="password_icon">
+                <input id="password" name="password" type="password" placeholder="password" class="flat_input_text" required>
+            </div>
+                <div id="login_regist">
+                    <input type="submit" value="登录" class="flat_button_future flat_button_future_rounded" id="login_btn">
+                    <button type="button" id="regist_btn">注册</button>
+                </div>
+
+            </form>
+        </fieldset>
+    </div>
+
+    <%--<div id="start_btn" class="flat_button_future" onclick="location.href='/html/duck_main.html'"><label id="start_btn_text">GET START</label></div>--%>
+
+</body>
+
+</html>
